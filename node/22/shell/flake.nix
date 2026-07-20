aacd6d6ea296084b1d1bb063a2b52d6204767f88
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2026-07-20 03:29:33 -0700

    first commit

---
+++ node/22/shell/flake.nix
@@ -0,0 +1,50 @@
+{
+  description        = "NodeJS Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nvim-config-pkg = {
+      url = "github:SawyerHopkins/nvim-config";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, nvim-config-pkg }:
+    let
+      supportedSystems = ["aarch64-linux"];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems f;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs_22
+              pkgs.corepack
+              pkgs.neovim
+              pkgs.ripgrep
+              pkgs.fd
+              pkgs.lazygit
+              pkgs.fzf
+              pkgs.jq
+              pkgs.tree-sitter
+              pkgs.bashInteractive
+            ];
+
+            shellHook = ''
+              echo "NodeJS Development Environment"
+              node --version
+
+              echo "Environment setup started"
+              mkdir -p /.config/nvim
+              cp -r ${nvim-config-pkg}/* /.config/nvim/
+              echo "Environment setup complete"
+            '';
+          };
+        }
+      );
+    };
+}
